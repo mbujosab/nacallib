ead1df051cd730b9aa5a948dbeb9efd63d2bf641
Marcos Bujosa <53706089+mbujosab@users.noreply.github.com>
2020-08-29 12:24:54 +0200

    Add files via upload

---
+++ nacal/__init__.py
@@ -0,0 +1,11 @@
+""" 
+nacal.
+
+Notación Asociativa para un curso de Álgebra Lineal (NAcAL).
+"""
+
+__version__ = "0.1.0889t7"
+__author__  = "Marcos Bujosa"
+__name__    = "nacal"
+
+from .nacal import *
